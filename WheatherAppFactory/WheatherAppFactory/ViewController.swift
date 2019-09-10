b28b46303b71894df460a5d9b0fd4991c9e2fabc
myCRObaM <matej.hetzel@gmail.com>
2019-09-10 12:50:43 +0200

    FirstCommit

---
+++ WheatherAppFactory/WheatherAppFactory/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  WheatherAppFactory
+//
+//  Created by Matej Hetzel on 10/09/2019.
+//  Copyright © 2019 Matej Hetzel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
